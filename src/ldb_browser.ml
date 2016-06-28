b85a8bece64091fc65e7ba339ed1d1bb59a6a4e8
Carmelo Piccione <carmelo.piccione@gmail.com>
2016-06-28 10:54:35 -0400

    cvs output

--- src/ldb_browser.ml
+++ src/ldb_browser.ml
@@ -5,12 +5,21 @@ let main tail max_ticks dbpath () =
   let nb_read = ref 0 in
   let f = if tail then LevelDB.rev_iter else LevelDB.iter in
   let db = LevelDB.open_db dbpath in
+  let ts_offset = ref Int64.zero in
   try
     f (fun ts data ->
         String.blit ts 0 buf 0 8;
         String.blit data 0 buf 8 16;
         let tick = Tick.IO.Bytes.read_tdts buf 0 in
-        Format.(fprintf std_formatter "%d %a@." !nb_read Tick.pp tick);
+        let rec_num = !nb_read in
+        let price  = Int64.to_float tick#p *. 1e-8 in
+        let vol = tick#v in
+        let side = match tick#side with `Buy -> 0 | `Sell -> 1 in
+        let ts =
+          if rec_num = 0 then Int64.zero else begin
+            let open Int64 in (tick#ts - !ts_offset) / Int64.of_float 1e5 end in
+        ts_offset := tick#ts;
+        Format.(fprintf std_formatter "%Ld %.2f %Ld %d@." ts price vol side);
         incr nb_read;
         Option.value_map max_ticks
           ~default:true ~f:(fun max_ticks -> not (!nb_read = max_ticks))
